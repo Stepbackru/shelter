84136253d5166a9fe0cbddd99fdf32660d5f0be4
Stepbackru <alex-ruba@bk.ru>
2020-10-19 20:59:40 +0300

    chore: start work with links

--- assets/components/js/constants.js
+++ assets/components/js/constants.js
@@ -3,6 +3,7 @@ export const BURGER = 'header__burger';
 export const BURGER_ICON = 'header__burger_icon';
 export const BURGER_ACTIVE = 'header__burger-active';
 export const MENU = 'menu';
+export const MENU_LINK = 'menu__link';
 export const MENU_ACTIVE = 'menu-active';
 export const BLACKOUT = 'blackout';
 export const LOGO = 'logo';
---
+++ assets/components/js/menu.js
@@ -0,0 +1,11 @@
+import * as C from '../js/constants.js';
+
+const menu = () => {
+  document.querySelector(`.${C.MENU}`).addEventListener('click', (e) => {
+    const item = e.target;
+    
+
+  });
+}
+
+export default menu;
