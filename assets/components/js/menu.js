efac3672d8a8d9836b0d302d5bbc55aa6a4f5d12
Stepbackru <alex-ruba@bk.ru>
2020-10-27 23:08:33 +0300

    feat: add disable links

--- assets/components/js/menu.js
+++ assets/components/js/menu.js
@@ -1,10 +1,17 @@
 import * as C from '../js/constants.js';
 
 const menu = () => {
+  const links = [...document.querySelectorAll(`.${C.MENU_LINK}`)];
+  const newLinks = links.slice(2);
+
   document.querySelector(`.${C.MENU}`).addEventListener('click', (e) => {
     const item = e.target;
-    
 
+    newLinks.forEach(el => {
+      if (item.innerText === el.innerText) {
+        e.preventDefault();
+      }
+    });
   });
 }
 
--- pages/main/script.js
+++ pages/main/script.js
@@ -1,5 +1,7 @@
 import burger from '../../assets/components/js/burger.js'
+import menu from '../../assets/components/js/menu.js'
 
 window.onload = () => {
   burger();
+  menu();
 };
